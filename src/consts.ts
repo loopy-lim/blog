c2046b6d1a05ed43f9cd0f9ca7a8ee6342ac122c
loopy-lim <loopy@jnu.ac.kr>
2024-05-23 22:58:41 +0900

    feat: reset blog

---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = "Loopy Blog";
+export const SITE_DESCRIPTION = "Loopy is User First";
--- static/Posts.ts
+++
@@ -1,1 +0,0 @@
-export const Posts = ["Econovation-Recruit-694df76e877d491098c1f702a92bc676"];
