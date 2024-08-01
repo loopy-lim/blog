08f42307c4afc8bf9646fae940ccc6d5cdefbe0d
loopy-lim <loopy@jnu.ac.kr>
2024-08-01 22:12:57 +0900

    feat: remove @fisch0920/use-dark-mode

--- lib/use-dark-mode.ts
+++ lib/use-dark-mode.ts
@@ -1,10 +1,13 @@
-import useDarkModeImpl from '@fisch0920/use-dark-mode'
+// import useDarkModeImpl from '@fisch0920/use-dark-mode'
+import { useDarkMode as useDarkModeImpl } from 'usehooks-ts'
+
+
 
 export function useDarkMode() {
-  const darkMode = useDarkModeImpl(false, { classNameDark: 'dark-mode' })
+  const { isDarkMode, toggle } = useDarkModeImpl({ defaultValue: false });
 
   return {
-    isDarkMode: darkMode.value,
-    toggleDarkMode: darkMode.toggle
+    isDarkMode: isDarkMode,
+    toggleDarkMode: toggle
   }
 }
