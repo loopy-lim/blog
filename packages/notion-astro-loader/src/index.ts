065568aae42215119034514be537069ba72aad66
loopy-lim <cotmd6203@naver.com>
2025-08-06 22:55:41 +0900

    feat: remove turborepo

--- packages/notion-astro-loader/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { type NotionLoaderOptions, notionLoader } from './loader.js';
-export { notionPageSchema } from './schemas/page.js';
-export * from './format.js';
-export * from './types.js';
--- packages/notion-astro-loader/src/schemas/index.ts
+++
@@ -1,3 +0,0 @@
-export { pageObjectSchema, notionPageSchema } from './page.js';
-export * as propertySchema from './raw-properties.js';
-export * as transformedPropertySchema from './transformed-properties.js';
