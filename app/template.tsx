1c47d3e4c986287fceea949eb4194feaad03a2c3
loopy-lim <loopy-lim@users.noreply.github.com>
2026-02-28 22:25:16 +0900

    refactor: PageTransition 컴포넌트 분리
    template.tsx에서 페이지 전환 애니메이션 로직을 별도 컴포넌트로 분리

--- app/template.tsx
+++ app/template.tsx
@@ -1,18 +1,5 @@
-'use client'
-
-import { motion } from 'framer-motion'
+import { PageTransition } from '@/components/layout/PageTransition'
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: 10 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ 
-        duration: 0.4, 
-        ease: 'easeOut'
-      }}
-    >
-      {children}
-    </motion.div>
-  )
+  return <PageTransition>{children}</PageTransition>
 }
